f36e81dbe0dffd16050d4c3107d3c4bd3d85d43b
Leyre <113433045+lgarciarob@users.noreply.github.com>
2022-10-04 13:41:09 +0200

    Ejercicio2

---
+++ Ejercicio2.py
@@ -0,0 +1,4 @@
+nombre_usuario = input("introduce tu nombre completo: \n")
+print ("Nombre en minúsculas: ", nombre_usuario.lower(), end="\n")
+print ("Nombre en mayúsculas: ", nombre_usuario.upper(), end="\n")
+print ("Nombre en mayusculas y minusculas: ", nombre_usuario.title(), end="\n")
