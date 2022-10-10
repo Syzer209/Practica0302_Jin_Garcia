d9ed6778accfa2023c2c70ab6228a83e5f644975
Leyre <113433045+lgarciarob@users.noreply.github.com>
2022-10-10 11:41:14 +0200

    Ejercicio9

---
+++ Ejercicio9.py
@@ -0,0 +1,4 @@
+fecha = input("¿Cuál es tu fecha de nacimiento? dd/mm/aaaa:" "\n")
+print("tu día es: ", fecha.split("/")[0])
+print("tu mes es: ", fecha.split("/")[1])
+print("tu año es:", fecha.split("/")[2])
